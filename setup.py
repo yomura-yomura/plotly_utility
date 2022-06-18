9228b68e4921fc8857ccabf5259aecbf7644c56c
yugo.omura <yomura1008@gmail.com>
2022-06-19 03:01:27 +0900

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='plotly_utility',
-    version='1.9.1',
+    version='1.10.0',
     description='',
     author='yomura',
     author_email='yomura@hoge.jp',
